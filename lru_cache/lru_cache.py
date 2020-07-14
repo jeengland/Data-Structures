c2323ec0faf0ee63216c7ed464b5ae236de07970
unknown <jacob.e.england@gmail.com>
2020-07-14 15:56:13 -0400

    Added LRU cache solution

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,13 +1,18 @@
+from doubly_linked_list.doubly_linked_list import DoublyLinkedList
+"""
+Our LRUCache class keeps track of the max number of nodes it
+can hold, the current number of nodes it is holding, a doubly-
+linked list that holds the key-value entries in the correct
+order, as well as a storage dict that provides fast access
+to every node stored in the cache.
+"""
+
+
 class LRUCache:
-    """
-    Our LRUCache class keeps track of the max number of nodes it
-    can hold, the current number of nodes it is holding, a doubly-
-    linked list that holds the key-value entries in the correct
-    order, as well as a storage dict that provides fast access
-    to every node stored in the cache.
-    """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.length = 0
+        self.storage = DoublyLinkedList()
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +22,13 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        current = self.storage.head
+        while current is not None:
+            if current.value[0] == key:
+                self.storage.move_to_front(current)
+                return current.value[1]
+            current = current.next
+        return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +41,18 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        current = self.storage.head
+        replaced = False
+        while current is not None and replaced is not True:
+            if current.value[0] == key:
+                current.value[1] = value
+                self.storage.move_to_front(current)
+                replaced = True
+            current = current.next
+
+        if self.length == self.limit and replaced is not True:
+            self.storage.remove_from_tail()
+            self.storage.add_to_head([key, value])
+        elif replaced is not True:
+            self.storage.add_to_head([key, value])
+            self.length = self.storage.length
