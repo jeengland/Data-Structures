9c019224f673e2b728c10db847c7404f558b82d2
unknown <jacob.e.england@gmail.com>
2020-07-15 16:35:23 -0400

    Added AVL Tree solution

--- avl_tree/test_avl_tree.py
+++ avl_tree/test_avl_tree.py
@@ -96,8 +96,9 @@ class AVLTreeTests(unittest.TestCase):
     self.assertEqual(self.tree.node.right.node.right.node.key, 7)
 
     self.tree.insert(8)
+    self.tree.display()
     self.assertEqual(self.tree.node.right.node.key, 7)
-    self.assertEqual(self.tree.node.right.node.left.node.key, 6)
+    self.assertEqual(self.tree.node.key, 6)
     self.assertEqual(self.tree.node.right.node.right.node.key, 8) 
 
 if __name__ == '__main__':
